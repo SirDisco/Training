0df296120bc4217cece42d5e421931d8c1bed42d
Joseph Walker <Joseph.Walker@softwire.com>
2021-09-28 09:31:30 +0100

    Created a working FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -7,7 +7,26 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             for (int i = 1; i <= 100; i++)
-                Console.WriteLine(i);
+            {
+                bool hasWritten = false;
+
+                if (i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                    hasWritten = true;
+                }
+                
+                if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                    hasWritten = true;
+                }
+                
+                if (!hasWritten)
+                    Console.Write(i);
+                
+                Console.WriteLine();
+            }
         }
     }
 }

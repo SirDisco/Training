eeca00bd8d14ec25bdbcc1e7ee1d12aca8cb5504
Joseph Walker <Joseph.Walker@softwire.com>
2021-09-28 09:46:27 +0100

    Extracted individual FizzBuzz 'turn' to function and used tuples to easily add more

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,31 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz
 {
     class Program
     {
+        private static Tuple<int, string>[] conversion =
+        {
+            Tuple.Create(3, "Fizz"),
+            Tuple.Create(5, "Buzz"),
+            Tuple.Create(7, "Bang")
+        };
+    
+        static void PrintCorrectFizz(int value)
+        {
+            bool hasPrintedWord = false;
+            
+            // Check against all Fizz/Buzz entries in array
+            foreach (var entry in conversion)
+            {
+                if ((value % entry.Item1) == 0)
+                {
+                    Console.Write(entry.Item2);
+                    hasPrintedWord = true;
+                }
+            }
+            
+            if (!hasPrintedWord)
+                Console.Write(value);
+            
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             for (int i = 1; i <= 100; i++)
             {
-                bool hasWritten = false;
-
-                if (i % 3 == 0)
-                {
-                    Console.Write("Fizz");
-                    hasWritten = true;
-                }
-                
-                if (i % 5 == 0)
-                {
-                    Console.Write("Buzz");
-                    hasWritten = true;
-                }
-                
-                if (!hasWritten)
-                    Console.Write(i);
-                
-                Console.WriteLine();
+                PrintCorrectFizz(i);
             }
         }
     }

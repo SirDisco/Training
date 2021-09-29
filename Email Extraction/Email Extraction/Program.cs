6e1f720f0619b1f36e8187b064101fe549893b33
Joseph Walker <Joseph.Walker@softwire.com>
2021-09-29 09:50:33 +0100

    Added Naive approach to counting softwire.com emails

--- Email Extraction/Email Extraction/Program.cs
+++ Email Extraction/Email Extraction/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace Email_Extraction
 {
     class Program
     {
+        static bool NaiveApproach(string email)
+        {
+            for (int i = 0; i < email.Length; i++)
+                if (email.Substring(i, 13) == "@softwire.com")
+                    return true;
+
+            return false;
+        }
+        
         static void Main(string[] args)
         {
+            string emailFile = "emails.??";
+
+            int numberOfSoftwireEmails = 0;
             
+            using (StreamReader reader = File.OpenText(emailFile))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    numberOfSoftwireEmails += (NaiveApproach(line) ? 1 : 0);
+            }
+            
+            Console.WriteLine(numberOfSoftwireEmails);
         }
     }
 }

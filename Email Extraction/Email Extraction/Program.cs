af659da03d5af3a3b4e3c00bb49fe48da6b5deba
Joseph Walker <Joseph.Walker@softwire.com>
2021-09-29 11:37:09 +0100

    Added dictionary approach to print all domains and their frequency

--- Email Extraction/Email Extraction/Program.cs
+++ Email Extraction/Email Extraction/Program.cs
@@ -1,33 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Email_Extraction
 {
     class Program
     {
-        static bool NaiveApproach(string email)
+        static void NaiveApproach(StreamReader text)
         {
-            for (int i = 0; i < email.Length; i++)
-                if (email.Substring(i, 13) == "@softwire.com")
-                    return true;
+            int totalSoftwireEmails = 0;
 
-            return false;
-        }
-        
-        static void Main(string[] args)
-        {
-            string emailFile = "emails.??";
-
-            int numberOfSoftwireEmails = 0;
-            
-            using (StreamReader reader = File.OpenText(emailFile))
+            string line;
+            while ((line = text.ReadLine()) != null)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                    numberOfSoftwireEmails += (NaiveApproach(line) ? 1 : 0);
+                for (int i = 0; i < line.Length - 13; i++)
+                    if (line.Substring(i, 13) == "@softwire.com")
+                        totalSoftwireEmails += 1;
             }
             
-            Console.WriteLine(numberOfSoftwireEmails);
+            Console.WriteLine("Naive Approach: " + totalSoftwireEmails.ToString() + " softwire emails");
+        }
+
+        static void RegexApproach(StreamReader text)
+        {
+            Regex pattern = new Regex(@"(\w+(?i)@softwire.com(?-i))\b");
+
+            int totalSoftwireEmails = pattern.Matches(text.ReadToEnd()).Count;
+            
+            Console.WriteLine("Regex Approach: " + totalSoftwireEmails.ToString() + " softwire emails");
+        }
+
+        static void DictionaryApproach(StreamReader text)
+        {
+            string wholeText = text.ReadToEnd();
+
+            // Regex emailRegex = new Regex(@"[a-zA-Z0-9-_.]+(@[a-zA-Z0-9-]+)\.\w+");
+            Regex emailRegex = new Regex(@"[a-zA-Z0-9-_.]+(@[a-zA-Z0-9-.]+)");
+
+            MatchCollection domainCollection = emailRegex.Matches(wholeText);
+
+            var domainCount = new Dictionary<string, int>();
+
+            foreach (Match domain in domainCollection)
+            {
+                string domainString = domain.Groups[1].Value;
+
+                if (!domainCount.ContainsKey(domainString))
+                    domainCount[domainString] = 1;
+                else
+                    domainCount[domainString]++;
+            }
+
+            int counter = 1;
+
+            int numberToDisplay = 1000;
+            int threshold = 0;
+
+            foreach (var domain in domainCount.OrderBy(key => key.Value).Reverse())
+            {
+                if (domain.Value >= threshold)
+                {
+                    Console.WriteLine($"Number of {domain.Key} emails: {domain.Value}");
+                    counter++;
+                }
+
+                if (counter > numberToDisplay)
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string emailFile = "sample.txt";
+
+            StreamReader reader = File.OpenText(emailFile);
+
+            NaiveApproach(reader);
+
+            reader.BaseStream.Position = 0;
+            RegexApproach(reader);
+            
+            reader.BaseStream.Position = 0;
+            DictionaryApproach(reader);
         }
     }
 }

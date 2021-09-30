a5f3ed80017f06c1942d9866e5bd053c4ab0401a
Joseph Walker <Joseph.Walker@softwire.com>
2021-09-30 14:14:43 +0100

    Created transaction class with constructor

---
+++ Support Bank/Support Bank/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Support_Bank
+{
+    public class Transaction
+    {
+        public Transaction(ref Account sender, ref Account receiver, float amount, string narrative, DateTime date)
+        {
+            m_Sender = sender;
+            m_Receiver = receiver;
+
+            m_Amount = amount;
+            m_Narrative = narrative;
+            m_Date = date;
+        }
+
+        private Account m_Sender;
+        private Account m_Receiver;
+
+        private float m_Amount;
+        private string m_Narrative;
+        private DateTime m_Date;
+    }
+}
